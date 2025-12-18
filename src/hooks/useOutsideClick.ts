8356a023b1166893c4a01b1b5936b140fd22b9ac
한지은 <hjn510450@gmail.com>
2025-12-19 01:00:14 +0900

    ✨ Feat: 외부 클릭 감지 커스텀 훅 useOutsideClick 구현

---
+++ src/hooks/useOutsideClick.ts
@@ -0,0 +1,21 @@
+import { useEffect } from 'react';
+
+export function useOutsideClick<T extends HTMLElement | null>(
+  ref: React.RefObject<T>,
+  callback: () => void,
+) {
+  useEffect(() => {
+    function handleClick(event: MouseEvent) {
+      if (!ref.current) return;
+
+      if (!ref.current.contains(event.target as Node)) {
+        callback();
+      }
+    }
+
+    document.addEventListener('pointerdown', handleClick);
+    return () => {
+      document.removeEventListener('pointerdown', handleClick);
+    };
+  }, [ref, callback]);
+}
