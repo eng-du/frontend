32a0085337e2e6e59b7707b9d624c8abc5f4c936
한지은 <hjn510450@gmail.com>
2025-12-20 12:56:15 +0900

    🐛 Fix: Label 컴포넌트 border 오타 수정

--- src/features/home/components/engdu-cards-section/Label.tsx
+++ src/features/home/components/engdu-cards-section/Label.tsx
@@ -11,8 +11,8 @@ function Label({ type }: LabelProps) {
       className={clsx(
         'rounded-2xl border bg-[#FFF0E1] px-3 py-1 text-12 font-bold',
         type === '상황극'
-          ? 'boder-[#EFBF04] bg-[#FFFFCC] text-[#EFBF04]'
-          : 'boder-[#FF7518] bg-[#FFF0E1] text-[#FF7518]',
+          ? 'border-[#EFBF04] bg-[#FFFFCC] text-[#EFBF04]'
+          : 'border-[#FF7518] bg-[#FFF0E1] text-[#FF7518]',
       )}
     >
       {type}
