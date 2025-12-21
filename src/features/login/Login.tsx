b4e4ff42c2909519830eedbffa889974f82859ec
한지은 <hjn510450@gmail.com>
2025-12-21 16:57:08 +0900

    ✨ Feat: Login 컴포넌트 UI 구현

---
+++ src/features/login/Login.tsx
@@ -0,0 +1,27 @@
+import Button from '@/components/Button';
+import Card from '@/components/Card';
+import GoogleIcon from '@/assets/icons/google.svg?react';
+import EngduFullBookIcon from '@/assets/icons/engdu-full-book.svg?react';
+
+function Login() {
+  return (
+    <div className="flex min-h-dvh items-center justify-center">
+      <Card className="w-107.5 items-center py-16">
+        <EngduFullBookIcon />
+        <div className="flex flex-col items-center gap-3">
+          <div className="font-pinkfong text-36">잉듀</div>
+          <div className="text-center text-text-secondary">
+            AI와 함께 즐거운
+            <br /> 영어 학습을 시작해보세요!
+          </div>
+        </div>
+        <Button type="primary" style="ghost">
+          <GoogleIcon />
+          <div className="flex-1">구글로 시작하기</div>
+        </Button>
+      </Card>
+    </div>
+  );
+}
+
+export default Login;
