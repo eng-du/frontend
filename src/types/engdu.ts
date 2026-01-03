b8637776ce69bebace13baf57fe5856d40b49e31
한지은 <hjn510450@gmail.com>
2026-01-03 16:40:41 +0900

    ✨ Feat: 잉듀 학습 화면을 나타내는 EngduLearning 컴포넌트 구현

--- src/types/engdu.ts
+++ src/types/engdu.ts
@@ -1,7 +1,22 @@
+import type { EngduQuestion } from './quiz';
+
 export interface EngduSummary {
   id: number;
   type: '지문' | '상황극';
   title: string;
   createdAt: Date;
   progress: number;
+}
+
+export interface DetailEngdu {
+  engduId: number;
+  title: string;
+  topic: string;
+  articles: EngduArticle[];
+  questions: EngduQuestion[];
+}
+
+export interface EngduArticle {
+  content: string;
+  translation: string;
 }
