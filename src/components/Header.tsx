6fb7cc462358f1a194ea685a29e1a7f0a3b9cf14
한지은 <hjn510450@gmail.com>
2026-01-07 23:00:15 +0900

    ✨ Feat: 로그인 여부에 따른 조건부 렌더링 적용

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,43 @@
+import { useAuth } from '@/hooks/useAuth';
+import { Link } from 'react-router';
+import { logout } from '@/api/auth';
+
 function Header() {
+  const { user, loading, clearAuth } = useAuth();
+
+  if (loading) return null;
+
   return (
     <header className="fixed top-0 left-0 z-100 flex h-15 w-full items-center justify-between border-b-2 border-border-default bg-surface-weak px-20">
-      <div className="flex items-center gap-2">
-        <img src="logo.svg" alt="로고" className="w-9" />
-        <div className="font-pinkfong text-20">잉듀</div>
-      </div>
+      <Link to={'/'}>
+        <div className="flex items-center gap-2">
+          <img src="logo.svg" alt="로고" className="w-9" />
+          <div className="font-pinkfong text-20">잉듀</div>
+        </div>
+      </Link>
       <ul className="flex gap-5 text-12 text-text-secondary">
-        <li>마이페이지</li>
-        <li>로그아웃</li>
+        {user ? (
+          <>
+            <li>{user.name}</li>
+            <li
+              className="cursor-pointer"
+              onClick={async () => {
+                try {
+                  await logout();
+                  clearAuth();
+                } catch (e) {
+                  console.error(e);
+                }
+              }}
+            >
+              로그아웃
+            </li>
+          </>
+        ) : (
+          <li>
+            <Link to="/login">로그인</Link>
+          </li>
+        )}
       </ul>
     </header>
   );
