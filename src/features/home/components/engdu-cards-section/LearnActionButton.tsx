f52c7c2eab77960467cc7708fbfa5654e8f1480c
한지은 <hjn510450@gmail.com>
2026-01-28 23:04:23 +0900

    ✨ Feat: 잉듀 학습 페이지 라우트 추가

--- src/features/home/components/engdu-cards-section/LearnActionButton.tsx
+++ src/features/home/components/engdu-cards-section/LearnActionButton.tsx
@@ -1,17 +1,21 @@
 import Button from '@/components/Button';
 import RetryIcon from '@/assets/icons/rotate-ccw.svg?react';
 import PlayIcon from '@/assets/icons/play.svg?react';
+import { Link } from 'react-router';
 
 interface LearnActionButtonProps {
+  engduId: number;
   progress: number;
 }
 
-function LearnActionButton({ progress }: LearnActionButtonProps) {
+function LearnActionButton({ engduId, progress }: LearnActionButtonProps) {
   return (
-    <Button>
-      {progress === 100 ? <RetryIcon /> : <PlayIcon />}
-      <div>{progress === 100 ? '다시' : '이어서'} 학습하기</div>
-    </Button>
+    <Link to={`/learning/${engduId}`} className='w-full'>
+      <Button>
+        {progress === 100 ? <RetryIcon /> : <PlayIcon />}
+        <div>{progress === 100 ? '다시' : '이어서'} 학습하기</div>
+      </Button>
+    </Link>
   );
 }
 
