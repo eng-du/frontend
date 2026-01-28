749e78f12f55ebb706dedf241a74ed1b51737b88
한지은 <hjn510450@gmail.com>
2026-01-28 23:06:54 +0900

    💄 Style: ProgressHeader 컴포넌트의 제목 라인 수 지정(2)

--- src/features/engdu-learning/components/progress-header/ProgressHeader.tsx
+++ src/features/engdu-learning/components/progress-header/ProgressHeader.tsx
@@ -10,7 +10,7 @@ interface ProgressHeaderProps {
 function ProgressHeader({ title, step, setStep, isQuestionsCorrected }: ProgressHeaderProps) {
   return (
     <div className="grid h-35 w-full grid-cols-[7fr_5fr] gap-10 bg-surface-default px-25 pt-10">
-      <div className="self-center font-pinkfong text-36">{title}</div>
+      <div className="self-center font-pinkfong text-36 line-clamp-2">{title}</div>
       <QuizStepper step={step} setStep={setStep} isQuestionsCorrected={isQuestionsCorrected} />
     </div>
   );
