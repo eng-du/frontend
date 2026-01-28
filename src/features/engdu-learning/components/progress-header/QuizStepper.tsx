b6a87048bf3c17023704d64c808daa5b9c05ae3d
한지은 <hjn510450@gmail.com>
2026-01-28 23:07:56 +0900

    💄 Style: 풀 수 있는 상태이나 풀지 않은 퀴즈의 QuizStepIndicator UI 수정

--- src/features/engdu-learning/components/progress-header/QuizStepper.tsx
+++ src/features/engdu-learning/components/progress-header/QuizStepper.tsx
@@ -9,15 +9,20 @@ interface QuizStepperProps {
 function QuizStepper({ step, setStep, isQuestionsCorrected }: QuizStepperProps) {
   return (
     <div className="flex items-center gap-4 self-center">
-      {isQuestionsCorrected.map((isCorrected, idx) => (
-        <QuizStepIndicator
-          key={idx}
-          step={idx}
-          isLocked={!isCorrected}
-          isActive={step === idx}
-          setStep={setStep}
-        />
-      ))}
+      {isQuestionsCorrected.map((isCorrected, idx) => {
+        const isReachable = idx === 0 || isQuestionsCorrected[idx - 1];
+
+        return (
+          <QuizStepIndicator
+            key={idx}
+            step={idx}
+            isLocked={!isReachable}
+            isCorrected={isCorrected}
+            isActive={step === idx}
+            setStep={setStep}
+          />
+        );
+      })}
     </div>
   );
 }
